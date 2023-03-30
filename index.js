91efc10cb273caa6a1abdcdf5d8b59d289bcbf08
heisenberg550 <150204095@kku.edu.tr>
2023-03-30 18:02:11 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+  let messagesArray = [];
+
+  for (let i = 0; i < namesArray.length; i++) {
+    let message = `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`;
+    messagesArray.push(message);
+  }
+
+  return messagesArray;
+}
+
+const countDown = function (num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+};
